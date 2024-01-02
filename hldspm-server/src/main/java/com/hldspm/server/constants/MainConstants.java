50c3d8d43c49355e119078f86fb6a0f8ea96c494
mrglaster <pristavkaegor03@gmail.com>
2024-01-02 13:14:20 +0800

    Old developments removed

--- hldspm-server/src/main/java/com/hldspm/server/constants/MainConstants.java
+++
@@ -1,9 +0,0 @@
-package com.hldspm.server.constants;
-
-import java.util.List;
-
-/**Bunch of few constants*/
-public class MainConstants {
-    public static List<String> games = List.of(new String[]{"hl", "cs", "dod", "cscz", "op4", "universal"});
-    public  static List<String> uploadTypes = List.of(new String[]{"plugin", "map"});
-}
--- hldspm-server/src/main/java/com/hldspm/server/database/data_processor/utils/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.hldspm.server.database.data_processor.utils;
-
-public class Utils {
-    /**Wraps string with single quotes*/
-    public static String wrapWithQuotes(Object obj){
-        return "'" + obj.toString() + "'";
-    }
-}
